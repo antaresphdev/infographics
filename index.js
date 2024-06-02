a8658fbe1a8c508d92f6b7feee07883f10630618
Francis Rubio <francis.rubio@dice205.com>
2024-06-02 14:08:32 +0800

    Add new infographics

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sizeOf = require('image-size')
 const { stringify } = require('yaml')
 
 const BASE_PATH = 'infographics'
-const YAML_PATH = 'index.yaml'
+const YAML_PATH = `yaml/${Date.now()}.yaml`
 
 const slidesets = fs.readdirSync(BASE_PATH)
 const details = slidesets
@@ -13,7 +13,10 @@ const details = slidesets
     const files = fs.readdirSync(filepath)
     const details = {
       title: '',
+      description: '',
+      prerequisites: [],
       path: folder,
+      tags: [],
       slides: files.map(file => getImageDetails(filepath, file))
     }
 
@@ -22,7 +25,7 @@ const details = slidesets
 const yaml = stringify(details)
 
 fs.writeFileSync(YAML_PATH, yaml, { encoding: 'utf-8', flag: 'w' })
-console.log(`${details.length} total slidesets written to YAML file.`)
+console.log(`${details.length} total slidesets written to YAML file ./${YAML_PATH}.`)
 
 function getImageDetails(basepath, file) {
   const dimensions = sizeOf(path.join(basepath, file))
